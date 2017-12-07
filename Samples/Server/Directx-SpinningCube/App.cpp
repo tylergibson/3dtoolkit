a9a26a33187fdc23ab30da8db20da6e9de60ea57
bengreenier <begreen@microsoft.com>
2017-12-06 19:52:46 -0500

    multi-peer multi-stream working, no input, no window handling

--- Samples/Server/Directx-SpinningCube/App.cpp
+++ Samples/Server/Directx-SpinningCube/App.cpp
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <shellapi.h>
 #include <fstream>
-#include <atomic>
 
 #include "DeviceResources.h"
 #include "CubeRenderer.h"
@@ -20,6 +19,7 @@
 #include "config_parser.h"
 #include "directx_buffer_capturer.h"
 #include "service/render_service.h"
+#include "multi_peer_conductor.hpp"
 #endif // TEST_RUNNER
 
 // Required app libs
@@ -56,158 +56,6 @@ VideoTestRunner*	g_videoTestRunner = nullptr;
 #endif // TESTRUNNER
 
 #ifndef TEST_RUNNER
-
-
-struct SingleInstanceViewData
-{
-	// dirextx vectors must be 16byte aligned
-	// so we override the operators to support
-	// new-ing this class
-
-	void* operator new(size_t i)
-	{
-		return _mm_malloc(i, 16);
-	}
-
-	void operator delete(void* p)
-	{
-		_mm_free(p);
-	}
-
-	bool IsZeroed()
-	{
-		return DirectX::XMVector3Equal(lookAt, DirectX::XMVectorZero()) &&
-			DirectX::XMVector3Equal(up, DirectX::XMVectorZero()) &&
-			DirectX::XMVector3Equal(eye, DirectX::XMVectorZero());
-	}
-
-	DirectX::XMVECTORF32 lookAt;
-	DirectX::XMVECTORF32 up;
-	DirectX::XMVECTORF32 eye;
-};
-
-struct SingleInstance
-{
-	std::atomic_bool shouldProcessEvents;
-	rtc::scoped_refptr<Conductor> conductor;
-	PeerConnectionClient* client;
-	DirectXBufferCapturer* bufferCapturer;
-	std::shared_ptr<SingleInstanceViewData> mostRecentView;
-};
-
-void CreateSingleInstanceThread(const std::string& instanceName,
-	DeviceResources* deviceResources,
-	MainWindow* wnd,
-	std::vector<std::shared_ptr<SingleInstance>>& instanceList)
-{
-	rtc::Event allocationCompleted(true, false);
-
-	// TODO(bengreenier): are we cool new-ing this into oblivion?
-	new std::thread([&]
-	{
-		rtc::Win32Thread threadWrapper;
-		rtc::ThreadManager::Instance()->SetCurrentThread(&threadWrapper);
-
-		std::shared_ptr<SingleInstanceViewData> mostRecentView(new SingleInstanceViewData());
-
-		InputDataHandler handler([&](const std::string& message)
-		{
-			char type[256];
-			char body[1024];
-			Json::Reader reader;
-			Json::Value msg = NULL;
-			reader.parse(message, msg, false);
-
-			if (msg.isMember("type") && msg.isMember("body"))
-			{
-				strcpy(type, msg.get("type", "").asCString());
-				strcpy(body, msg.get("body", "").asCString());
-				std::istringstream datastream(body);
-				std::string token;
-
-				if (strcmp(type, "camera-transform-lookat") == 0)
-				{
-					// Eye point.
-					getline(datastream, token, ',');
-					float eyeX = stof(token);
-					getline(datastream, token, ',');
-					float eyeY = stof(token);
-					getline(datastream, token, ',');
-					float eyeZ = stof(token);
-
-					// Focus point.
-					getline(datastream, token, ',');
-					float focusX = stof(token);
-					getline(datastream, token, ',');
-					float focusY = stof(token);
-					getline(datastream, token, ',');
-					float focusZ = stof(token);
-
-					// Up vector.
-					getline(datastream, token, ',');
-					float upX = stof(token);
-					getline(datastream, token, ',');
-					float upY = stof(token);
-					getline(datastream, token, ',');
-					float upZ = stof(token);
-
-					const DirectX::XMVECTORF32 lookAt = { focusX, focusY, focusZ, 0.f };
-					const DirectX::XMVECTORF32 up = { upX, upY, upZ, 0.f };
-					const DirectX::XMVECTORF32 eye = { eyeX, eyeY, eyeZ, 0.f };
-
-					mostRecentView->lookAt = lookAt;
-					mostRecentView->up = up;
-					mostRecentView->eye = eye;
-				}
-			}
-		});
-
-		PeerConnectionClient* client = new PeerConnectionClient(instanceName);
-
-		auto nvEncConfig = GlobalObject<NvEncConfig>::Get();
-
-		// Note: Conductor is responsible for cleaning up bufferCapturer object.
-		DirectXBufferCapturer* bufferCapturer = new DirectXBufferCapturer(
-			deviceResources->GetD3DDevice());
-
-		bufferCapturer->Initialize();
-
-		if (nvEncConfig->use_software_encoding)
-		{
-			bufferCapturer->EnableSoftwareEncoder();
-		}
-
-		auto webrtcConfig = GlobalObject<WebRTCConfig>::Get();
-
-		rtc::scoped_refptr<Conductor> conductor(new rtc::RefCountedObject<Conductor>(
-			client, bufferCapturer, wnd, webrtcConfig.get()));
-
-		conductor->SetInputDataHandler(&handler);
-		client->SetHeartbeatMs(webrtcConfig->heartbeat);
-
-		std::shared_ptr<SingleInstance> instance(new SingleInstance());
-		
-		instance->shouldProcessEvents.store(true);
-		instance->conductor = conductor;
-		instance->client = client;
-		instance->bufferCapturer = bufferCapturer;
-		instance->mostRecentView = mostRecentView;
-
-		instanceList.push_back(instance);
-
-		// inform the parent thread that allocation is complete
-		allocationCompleted.Set();
-
-		// process until stopped
-		while (instance->shouldProcessEvents.load())
-		{
-			threadWrapper.ProcessMessages(500);
-		}
-	});
-
-	// wait until allocation of this thread is complete before returning
-	allocationCompleted.Wait(rtc::Event::kForever);
-}
 
 bool AppMain(BOOL stopping)
 {
@@ -259,24 +107,21 @@ bool AppMain(BOOL stopping)
 			reinterpret_cast<void**>(frameBuffer.GetAddressOf()));
 	}
 
-	std::vector<std::shared_ptr<SingleInstance>> instances;
+	// Creates and initializes the buffer capturer.
+	// Note: Conductor is responsible for cleaning up bufferCapturer object.
+	shared_ptr<DirectXBufferCapturer> bufferCapturer(new DirectXBufferCapturer(
+		g_deviceResources->GetD3DDevice()));
 
-	// allocate threads
-	for (auto i = 0; i < 2; ++i)
+	bufferCapturer->Initialize();
+	if (nvEncConfig->use_software_encoding)
 	{
-		// create an instance, adding itself to instances and handles
-		// note: this is blocking until it's been added to the lists
-		CreateSingleInstanceThread(
-			"[" + std::to_string(i) + "]",
-			g_deviceResources,
-			&wnd,
-			instances
-		);
+		bufferCapturer->EnableSoftwareEncoder();
 	}
 
-	// reset the current thread in case CSIT changed it
-	// TODO(bengreenier): do we need to do this?
-	rtc::ThreadManager::Instance()->SetCurrentThread(&w32_thread);
+	// Initializes the conductor.
+	MultiPeerConductor cond(webrtcConfig, bufferCapturer);
+
+	cond.ConnectSignallingAsync("renderingserver_test");
 
 	// Main loop.
 	while (!stopping)
@@ -293,6 +138,11 @@ bool AppMain(BOOL stopping)
 		{
 			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 			{
+				if (msg.message == WM_CLOSE)
+				{
+					break;
+				}
+
 				if (!wnd.PreTranslateMessage(&msg))
 				{
 					TranslateMessage(&msg);
@@ -301,66 +151,24 @@ bool AppMain(BOOL stopping)
 			}
 			else
 			{
-				if (std::any_of(instances.begin(), instances.end(), [](std::shared_ptr<SingleInstance> i)
+				ULONGLONG tick = GetTickCount64();
+				g_cubeRenderer->Update();
+				g_cubeRenderer->Render();
+				bufferCapturer->SendFrame(frameBuffer.Get());
+				g_deviceResources->Present();
+
+				// FPS limiter.
+				const int interval = 1000 / nvEncConfig->capture_fps;
+				ULONGLONG timeElapsed = GetTickCount64() - tick;
+				DWORD sleepAmount = 0;
+				if (timeElapsed < interval)
 				{
-					return i->conductor->is_closing();
-				}))
-				{
-					break;
+					sleepAmount = interval - timeElapsed;
 				}
 
-				if (std::any_of(instances.begin(), instances.end(), [](std::shared_ptr<SingleInstance> i)
-				{
-					// TODO(bengreenier): do we need this second condition?
-					return i->conductor->connection_active() || i->client->is_connected();
-				}))
-				{
-					ULONGLONG tick = GetTickCount64();
-					g_cubeRenderer->Update();
-
-					for each (auto instance in instances)
-					{
-						auto mrv = instance->mostRecentView;
-						auto bc = instance->bufferCapturer;
-
-						// TODO(bengreenier): why does zero-ed crash?
-						if (!mrv->IsZeroed())
-						{
-							g_cubeRenderer->UpdateView(mrv->eye, mrv->lookAt, mrv->up);
-						}
-						g_cubeRenderer->Render();
-
-						bc->SendFrame(frameBuffer.Get());
-					}
-
-					// TODO(bengreenier): by doing just a present here, we will always see
-					// on our screen the render from the view of the last instance in the list
-					g_deviceResources->Present();
-
-					// FPS limiter.
-					const int interval = 1000 / nvEncConfig->capture_fps;
-					ULONGLONG timeElapsed = GetTickCount64() - tick;
-					DWORD sleepAmount = 0;
-					if (timeElapsed < interval)
-					{
-						sleepAmount = interval - timeElapsed;
-					}
-
-					Sleep(sleepAmount);
-				}
+				Sleep(sleepAmount);
 			}
 		}
-	}
-
-	// shutdown the instances
-	for each (auto instance in instances)
-	{
-		instance->shouldProcessEvents.store(false);
-
-		// TODO(bengreenier): we should add a "wait" here until the instance thread is complete
-
-		instance->conductor->Release();
-		delete instance->client;
 	}
 
 	rtc::CleanupSSL();
@@ -533,10 +341,10 @@ int WINAPI wWinMain(
 	// Creates and initializes the video test runner library.
 	g_videoTestRunner = new VideoTestRunner(
 		g_deviceResources->GetD3DDevice(),
-		g_deviceResources->GetD3DDeviceContext()); 
+		g_deviceResources->GetD3DDeviceContext());
 
 	g_videoTestRunner->StartTestRunner(g_deviceResources->GetSwapChain());
-	
+
 	// Main message loop.
 	MSG msg = { 0 };
 	while (WM_QUIT != msg.message)
@@ -556,7 +364,7 @@ int WINAPI wWinMain(
 			}
 
 			g_videoTestRunner->TestCapture();
-			if (g_videoTestRunner->IsNewTest()) 
+			if (g_videoTestRunner->IsNewTest())
 			{
 				delete g_cubeRenderer;
 				g_cubeRenderer = new CubeRenderer(g_deviceResources);
@@ -569,7 +377,7 @@ int WINAPI wWinMain(
 
 	return (int)msg.wParam;
 #else // TEST_RUNNER
-	
+
 	// setup the config parsers
 	ConfigParser::ConfigureConfigFactories();
 
